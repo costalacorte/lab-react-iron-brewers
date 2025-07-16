932aa9d114e2ea0c7971117fd2f739826709288e
Raquel Lacorte <costa.lacorte@gmail.com>
2025-07-16 22:49:30 +0100

    completed lab iron-brewers

--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,56 +1,42 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import Search from "../components/Search";
-import beersJSON from "./../assets/beers.json";
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function AllBeersPage() {
-  // Mock initial state, to be replaced by data from the API. Once you retrieve the list of beers from the Beers API store it in this state variable.
-  const [beers, setBeers] = useState(beersJSON);
+  const [beers, setBeers] = useState([]);
+  const [query, setQuery] = useState("");
 
+  useEffect(() => {
+    const url = query
+      ? `https://ih-beers-api2.herokuapp.com/beers/search?q=${query}`
+      : "https://ih-beers-api2.herokuapp.com/beers";
 
+    axios
+      .get(url)
+      .then((response) => {
+        setBeers(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching beers:", error);
+      });
+  }, [query]);
 
-  // TASKS:
-  // 1. Set up an effect hook to make a request to the Beers API and get a list with all the beers.
-  // 2. Use axios to make a HTTP request.
-  // 3. Use the response data from the Beers API to update the state variable.
-
-
-
-  // The logic and the structure for the page showing the list of beers. You can leave this as it is for now.
   return (
-    <>
-      <Search />
-
-      <div className="d-inline-flex flex-wrap justify-content-center align-items-center w-100 p-4">
-        {beers &&
-          beers.map((beer, i) => {
-            return (
-              <div key={i}>
-                <Link to={"/beers/" + beer._id}>
-                  <div className="card m-2 p-2 text-center" style={{ width: "24rem", height: "18rem" }}>
-                    <div className="card-body">
-                      <img
-                        src={beer.image_url}
-                        style={{ height: "6rem" }}
-                        alt={"image of" + beer.name}
-                      />
-                      <h5 className="card-title text-truncate mt-2">{beer.name}</h5>
-                      <h6 className="card-subtitle mb-3 text-muted">
-                        <em>{beer.tagline}</em>
-                      </h6>
-                      <p className="card-text">
-                        Created by: {beer.contributed_by}
-                      </p>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-      </div>
-    </>
+    <div>
+      <h1>All Beers</h1>
+      <input
+        type="text"
+        placeholder="Search beers..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {beers.map((beer) => (
+        <div key={beer._id}>
+          <img src={beer.image_url} alt={beer.name} style={{ height: "100px" }} />
+          <h3>{beer.name}</h3>
+          <p>{beer.tagline}</p>
+        </div>
+      ))}
+    </div>
   );
 }
 
